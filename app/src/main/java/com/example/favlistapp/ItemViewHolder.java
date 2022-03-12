daca60c9405a5090f0dd4400b675b48a7c0bdf9b
Vandana <vandana023ssm@gmail.com>
2022-03-12 23:30:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/favlistapp/ItemViewHolder.java
@@ -0,0 +1,24 @@
+package com.example.favlistapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView itemTextView;
+
+    public ItemViewHolder(View itemView) {
+
+        super(itemView);
+
+        itemTextView = itemView.findViewById(R.id.item_textView);
+
+
+    }
+
+
+}
